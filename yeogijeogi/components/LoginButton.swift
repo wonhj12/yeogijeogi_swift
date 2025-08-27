67ba182be29a1fc1e7b9c70d79b8a795b280ee53
wonhj12 <wonhj12@naver.com>
2025-08-27 21:43:44 +0900

    design: 로그인 버튼 컴포넌트 디자인 구현

---
+++ yeogijeogi/components/LoginButton.swift
@@ -0,0 +1,29 @@
+import SwiftUI
+
+struct LoginButton: View {
+    let type: LoginType
+
+    var body: some View {
+        Button {} label: {
+            HStack {
+                Image(type.icon)
+                    .frame(width: 20, height: 20)
+                Spacer()
+                Text(type.text)
+                    .foregroundStyle(.onSurface)
+                    .font(.callout)
+                Spacer()
+            }
+            .frame(height: 48)
+            .padding(.horizontal, 20)
+        }
+        .frame(maxWidth: .infinity)
+        .buttonStyle(.borderedProminent)
+        .tint(.container)
+        .cornerRadius(10)
+    }
+}
+
+#Preview {
+    LoginButton(type: .apple)
+}
