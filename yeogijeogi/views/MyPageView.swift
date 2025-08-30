076cbcbcaa87d6cd5a9c260919330154b6e008de
wonhj12 <wonhj12@naver.com>
2025-08-30 22:37:01 +0900

    design: 마이페이지 디자인 구현

---
+++ yeogijeogi/views/MyPageView.swift
@@ -0,0 +1,77 @@
+import SwiftUI
+
+struct MyPageView: View {
+    var body: some View {
+        NavigationStack {
+            VStack {
+                Text("여기저기님")
+                    .font(.title)
+                    .foregroundStyle(.onSurface)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                Spacer()
+                    .frame(height: 8)
+
+                Text("오늘도 열심히 산책을 하고 계시네요!")
+                    .font(.headline)
+                    .foregroundStyle(.onSurface)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                Spacer()
+                    .frame(height: 24)
+
+                SummaryCotainer(distance: 0, time: 0)
+                Spacer()
+                    .frame(height: 24)
+
+                Text("계정")
+                    .font(.headline)
+                    .foregroundStyle(.onSurface)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                Spacer()
+                    .frame(height: 16)
+
+                ButtonContainer(title: "닉네임", buttonText: "여기저기")
+                Spacer()
+                    .frame(height: 24)
+
+                Text("이용 안내")
+                    .font(.headline)
+                    .foregroundStyle(.onSurface)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                Spacer()
+                    .frame(height: 16)
+
+                SettingContainer()
+                Spacer()
+
+                HStack {
+                    Button {} label: {
+                        Text("회원탈퇴")
+                            .font(.caption)
+                            .foregroundStyle(.onSurfaceVariant)
+                    }
+                    Spacer()
+                        .frame(width: 16)
+
+                    Divider()
+                        .foregroundStyle(.onSurfaceVariant)
+                        .frame(height: 8)
+                    Spacer()
+                        .frame(width: 16)
+
+                    Button {} label: {
+                        Text("로그아웃")
+                            .font(.caption)
+                            .foregroundStyle(.onSurfaceVariant)
+                    }
+                }
+            }
+            .navigationTitle("마이페이지")
+            .navigationBarTitleDisplayMode(.inline)
+            .surface()
+        }
+    }
+}
+
+#Preview {
+    MyPageView()
+}
