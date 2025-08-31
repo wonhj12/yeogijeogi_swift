2f808645aa738bb4ab50f8efe6a1229563e2aa83
wonhj12 <wonhj12@naver.com>
2025-08-31 11:55:42 +0900

    feat: 샌책 코스 요약 정보를 재사용 가능하도록 컴포넌트화 - 이미지, 요약, 분위기/강도 슬라이더, 메모

--- yeogijeogi/views/WalkSaveView.swift
+++ yeogijeogi/views/WalkSaveView.swift
@@ -18,25 +18,7 @@ struct WalkSaveView: View {
                 Spacer()
                     .frame(height: 40)
 
-                ImageContainer()
-                Spacer()
-                    .frame(height: 24)
-
-                CourseInfoContainer()
-                Spacer()
-                    .frame(height: 24)
-
-                SliderContainer(title: "분위기가 어떘나요?", valueTexts: ["자연", "", "도시"])
-                Spacer()
-                    .frame(height: 24)
-
-                SliderContainer(title: "산책 강도는 어떘나요?", valueTexts: ["쉬움", "적당함", "어려움"])
-                Spacer()
-                    .frame(height: 24)
-
-                MemoContainer()
-                Spacer()
-                    .frame(height: 40)
+                CourseSummary()
 
                 LargeButton(title: "산책 코스 저장하기")
             }
