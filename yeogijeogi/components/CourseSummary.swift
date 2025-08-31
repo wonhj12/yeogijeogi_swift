05ddb208b17544e63bef351673a9e29fd19e28bb
wonhj12 <wonhj12@naver.com>
2025-08-31 23:54:15 +0900

    style: 오타 수정

--- yeogijeogi/components/CourseSummary.swift
+++ yeogijeogi/components/CourseSummary.swift
@@ -10,11 +10,11 @@ struct CourseSummary: View {
         Spacer()
             .frame(height: 24)
 
-        SliderContainer(title: "분위기가 어떘나요?", valueTexts: ["자연", "", "도시"])
+        SliderContainer(title: "분위기가 어땠나요?", valueTexts: ["자연", "", "도시"])
         Spacer()
             .frame(height: 24)
 
-        SliderContainer(title: "산책 강도는 어떘나요?", valueTexts: ["쉬움", "적당함", "어려움"])
+        SliderContainer(title: "산책 강도는 어땠나요?", valueTexts: ["쉬움", "적당함", "어려움"])
         Spacer()
             .frame(height: 24)
 
