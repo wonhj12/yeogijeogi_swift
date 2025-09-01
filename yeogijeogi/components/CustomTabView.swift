723381bb26d1d751052cc451eea37de76e221a46
wonhj12 <wonhj12@naver.com>
2025-09-01 13:49:03 +0900

    feat: 커스텀 탭뷰 구현 - 커스텀 아이콘 사용 - 상단 corner radius 적용

---
+++ yeogijeogi/components/CustomTabView.swift
@@ -0,0 +1,39 @@
+import SwiftUI
+
+struct CustomTabView: View {
+    @Binding var selectedTab: Tab
+
+    var body: some View {
+        HStack {
+            Spacer()
+
+            ForEach(Tab.allCases, id: \.self) { tab in
+                Button {
+                    self.selectedTab = tab
+                } label: {
+                    VStack {
+                        Image(tab.icon)
+                            .tint(selectedTab == tab ? .onSurface : .onSurfaceVariant)
+                        Spacer()
+                            .frame(height: 8)
+
+                        Text(tab.title)
+                            .font(.caption)
+                            .foregroundStyle(selectedTab == tab ? .onSurface : .onSurfaceVariant)
+                    }
+                }
+                .frame(width: 52)
+
+                Spacer()
+            }
+        }
+        .frame(maxWidth: .infinity)
+        .frame(height: 90)
+        .background(.container)
+        .clipShape(.rect(topLeadingRadius: 20, topTrailingRadius: 20))
+    }
+}
+
+#Preview {
+    CustomTabView(selectedTab: .constant(.walk))
+}
