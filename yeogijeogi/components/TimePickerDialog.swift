8ff506c3b6c0024f4a6402c07b42c45a0b2c7ba3
wonhj12 <wonhj12@naver.com>
2025-09-05 16:27:54 +0900

    feat: 시간 값 지정하는 TimePickerDialog 구현 - 다이얼로그처럼 켜지도록 modifier 구현

---
+++ yeogijeogi/components/TimePickerDialog.swift
@@ -0,0 +1,83 @@
+import SwiftUI
+
+struct TimePickerDialog: View {
+    @Binding var isPresented: Bool
+    @Binding var walkTime: Int
+
+    @State private var hour: Int = 0
+    @State private var minute: Int = 0
+
+    var body: some View {
+        VStack(alignment: .trailing) {
+            HStack(spacing: 40) {
+                CustomPicker(selection: $hour, type: .hour)
+                CustomPicker(selection: $minute, type: .minute)
+            }
+            .offset(x: -16)
+            .padding(.horizontal, 40)
+
+            Button {
+                walkTime = hour * 60 + minute
+                isPresented = false
+            } label: {
+                Text("완료")
+                    .font(.headline)
+                    .foregroundColor(.primaryGreen)
+            }
+            .padding(.bottom, 20)
+            .padding(.horizontal, 20)
+        }
+        .background(.surface)
+        .clipShape(.rect(cornerRadius: 20))
+        .onAppear {
+            hour = walkTime / 60
+            minute = walkTime % 60
+        }
+        .onChange(of: hour) { validateSelection() }
+        .onChange(of: minute) { validateSelection() }
+    }
+
+    // 0시간 0분으로 설정할 수 없도록 방지
+    private func validateSelection() {
+        if hour == 0, minute == 0 {
+            withAnimation(.easeInOut) {
+                minute = 5
+            }
+        }
+    }
+}
+
+#Preview {
+    TimePickerDialog(
+        isPresented: .constant(false),
+        walkTime: .constant(30)
+    )
+}
+
+struct TimePickerModifier: ViewModifier {
+    @Binding var isPresented: Bool
+    @Binding var walkTime: Int
+
+    func body(content: Content) -> some View {
+        content.overlay {
+            if isPresented {
+                ZStack {
+                    Color.onSurface.opacity(0.3)
+                        .ignoresSafeArea()
+                        .onTapGesture { isPresented = false }
+
+                    TimePickerDialog(
+                        isPresented: $isPresented,
+                        walkTime: $walkTime
+                    )
+                }
+            }
+        }
+    }
+}
+
+extension View {
+    func showTimePickerDialog(isPresented: Binding<Bool>, walkTime: Binding<Int>) -> some View {
+        modifier(TimePickerModifier(isPresented: isPresented, walkTime: walkTime))
+    }
+}
