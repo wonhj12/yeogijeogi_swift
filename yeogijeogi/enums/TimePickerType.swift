81424f95fce57cdf1b3eb4e8e57841bd620ad411
wonhj12 <wonhj12@naver.com>
2025-09-05 16:24:37 +0900

    feat: TimePicker에 사용될 enum 정의

---
+++ yeogijeogi/enums/TimePickerType.swift
@@ -0,0 +1,30 @@
+enum TimePickerType {
+    case hour, minute
+
+    var maxValue: Int {
+        switch self {
+        case .hour:
+            return 24
+        case .minute:
+            return 59
+        }
+    }
+
+    var stride: Int {
+        switch self {
+        case .hour:
+            return 1
+        case .minute:
+            return 5
+        }
+    }
+
+    var label: String {
+        switch self {
+        case .hour:
+            return "시간"
+        case .minute:
+            return "분"
+        }
+    }
+}
