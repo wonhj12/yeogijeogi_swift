de8920b006dc240cc9d2eb3d5691c065a361cada
wonhj12 <wonhj12@naver.com>
2025-09-14 09:36:37 +0900

    feat: firebase provider id를 LoginType로 변환하는 로직 구현

--- yeogijeogi/enums/LoginType.swift
+++ yeogijeogi/enums/LoginType.swift
@@ -4,6 +4,10 @@ enum LoginType {
     case google
     case apple
 
+    enum LoginTypeError: Error {
+        case unknownProvider(String)
+    }
+
     var text: String {
         switch self {
         case .google:
@@ -19,6 +23,17 @@ enum LoginType {
             return .googleLogo
         case .apple:
             return .appleLogo
+        }
+    }
+
+    static func fromProviderId(providerId: String) throws -> LoginType {
+        switch providerId {
+        case "google.com":
+            return .google
+        case "apple.com":
+            return .apple
+        default:
+            throw LoginTypeError.unknownProvider(providerId)
         }
     }
 }
