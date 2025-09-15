f3db2a9d2db5348f8924e61210646ba61744a7bf
wonhj12 <wonhj12@naver.com>
2025-09-15 23:41:30 +0900

    feat: 다이얼로그 최상위 enum 정의

--- yeogijeogi/enums/AnyDialog.swift
+++ yeogijeogi/enums/AnyDialog.swift
@@ -1,8 +1,15 @@
-//
-//  AnyDialog.swift
-//  yeogijeogi
-//
-//  Created by hajin on 9/15/25.
-//
+import SwiftUI
 
-import Foundation
+enum AnyDialog {
+    case dialog(type: DialogType, action: () -> Void = {})
+    case timePicker(walkTime: Binding<Int>)
+
+    var isBackgroundDismissable: Bool {
+        switch self {
+        case .dialog:
+            return false
+        case .timePicker:
+            return true
+        }
+    }
+}
