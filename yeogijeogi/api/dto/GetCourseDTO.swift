ea5a616c5ab4107247a8c088bde640da25bd64c3
wonhj12 <wonhj12@naver.com>
2025-09-19 15:23:43 +0900

    feat: GetCourseResDTO 구현 - CoursePreview 구현

--- yeogijeogi/api/dto/GetCourseDTO.swift
+++ yeogijeogi/api/dto/GetCourseDTO.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+struct CoursePreview: Codable {
+    var walkId: Int
+    var longitude: Double
+    var latitude: Double
+
+    enum CodingKeys: String, CodingKey {
+        case walkId = "walk_id"
+        case longitude
+        case latitude
+    }
+}
+
+struct GetCoursesResDTO: Codable {
+    var courses: [CoursePreview]
+}
