6448c99f7ffc8ba8843403c1293d7f008516cbac
wonhj12 <wonhj12@naver.com>
2025-09-19 15:26:01 +0900

    feat: Course 라우터 구현 - GET /course/ 구현

--- yeogijeogi/api/routers/CourseRouter.swift
+++ yeogijeogi/api/routers/CourseRouter.swift
@@ -5,4 +5,32 @@
 //  Created by hajin on 9/19/25.
 //
 
+import Alamofire
 import Foundation
+
+enum CourseRouter: RouterInterface {
+    case getCourses
+
+    var modulePath: String { "/course" }
+
+    var path: String {
+        switch self {
+        case .getCourses:
+            return "/"
+        }
+    }
+
+    var method: HTTPMethod {
+        switch self {
+        case .getCourses:
+            return .get
+        }
+    }
+
+    var parameters: Parameters? {
+        switch self {
+        case .getCourses:
+            return nil
+        }
+    }
+}
--- yeogijeogi/api/services/CourseService.swift
+++ yeogijeogi/api/services/CourseService.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+
+class CourseService {
+    static let shared = CourseService()
+
+    private init() {}
+
+    func getCourses(completion: @escaping (Result<GetCoursesResDTO, APIError>) -> Void) {
+        APIService.request(CourseRouter.getCourses, completion: completion)
+    }
+}
